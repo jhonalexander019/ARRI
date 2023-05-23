1438befffcceca48235b93abd8e03a13731a6597
jhonalexander019 <jhon.roa@unillanos.edu.co>
2023-05-22 22:55:44 -0500

    Error2

--- src/components/menu.jsx
+++ src/components/menu.jsx
@@ -4,7 +4,7 @@ import '../styles/Menu.module.css';
 export function Menu(){
     return (
         <ul  data-animation="center">
-            <li><Link to="/">Menu</Link></li>
+            <li><Link to="/">Inicio</Link></li>
             {/* <li><Link to="/About">Generar Reporte</Link></li> */}
             <li><Link to="/Sign">Iniciar Sesion</Link></li>
           </ul>
